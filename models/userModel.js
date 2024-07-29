67e9cd726e067e06f5b925c2e800a87733593285
Zeeshan Ali <alizeeshan0704@gmail.com>
2024-07-29 23:30:47 +0530

    Adding Changes

--- models/userModel.js
+++ models/userModel.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    firstName: { type: String },
+    lastName: { type: String},
     email: { type: String, required: true, unique: true },
     password: { type: String },
     googleId: { type: String },
